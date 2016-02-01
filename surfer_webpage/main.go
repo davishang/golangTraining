64fd5444ca3f8428c0b12afa732a97c70135cb0a
davishang <davishang@mail.fresnostate.edu>
2016-02-01 01:03:14 -0800

    edit surfer_webpage

--- surfer_webpage/main.go
+++ surfer_webpage/main.go
@@ -7,18 +7,23 @@ import (
 	"net/http"
 )
 
-func foo(res http.ResponseWriter, req *http.Request) {
-	io.WriteString(res, "Foo ran")
+func webpage(res http.ResponseWriter, req *http.Request) {
+	io.WriteString(res, "webpage ran")
 	tpl, err := template.ParseFiles("index.html")
 	if err != nil {
 		log.Fatalln(err)
 	}
-	http.ServeFile(res, req, "index.html")
+	http.ServeFile(res, req, "html/index.html")
 	tpl.Execute(res, nil)
 }
 
 func main() {
-	http.HandleFunc("/page/", foo)
+	//Handle css directory.
+	http.Handle("/css/",http.StripPrefix("/css",http.FileServer(http.Dir("css"))))
+	//Handle pic directory.
+	http.Handle("/pic/",http.StripPrefix("/pic",http.FileServer(http.Dir("pic"))))
+	//Handle index.html.
+	http.HandleFunc("/page/", webpage)
 	http.ListenAndServe(":8080", nil)
 }
 
