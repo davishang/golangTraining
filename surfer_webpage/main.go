cc8e194217dddd432a2e828aa62932a49e25c14b
davishang <davishang@mail.fresnostate.edu>
2016-02-01 10:38:27 -0800

    serving surfer_webpage on Go

--- surfer_webpage/main.go
+++ surfer_webpage/main.go
@@ -2,28 +2,27 @@ package main
 
 import (
 	"html/template"
-	"io"
+	//"io"
 	"log"
 	"net/http"
 )
 
-func webpage(res http.ResponseWriter, req *http.Request) {
-	io.WriteString(res, "webpage ran")
+func foo(res http.ResponseWriter, req *http.Request) {
+
+	//io.WriteString(res, "webpage ran")
 	tpl, err := template.ParseFiles("index.html")
 	if err != nil {
 		log.Fatalln(err)
 	}
-	http.ServeFile(res, req, "html/index.html")
+	//http.ServeFile(res, req, "index.html")
 	tpl.Execute(res, nil)
 }
 
 func main() {
-	//Handle css directory.
-	http.Handle("/css/",http.StripPrefix("/css",http.FileServer(http.Dir("css"))))
-	//Handle pic directory.
-	http.Handle("/pic/",http.StripPrefix("/pic",http.FileServer(http.Dir("pic"))))
-	//Handle index.html.
-	http.HandleFunc("/page/", webpage)
+	http.HandleFunc("/", foo)
+	http.Handle("/css/",http.StripPrefix("/css",http.FileServer(http.Dir("css") ) ) )
+	http.Handle("/pic/",http.StripPrefix("/pic",http.FileServer(http.Dir("pic") ) ) )
 	http.ListenAndServe(":8080", nil)
 }
 
+// localhost:8080
