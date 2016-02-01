5e3e7e4813111e6093f0304951952ff9e50d619a
davishang <davishang@mail.fresnostate.edu>
2016-01-31 23:53:36 -0800

    committing exercises1

---
+++ conditional_template/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"os"
+)
+
+type Person struct {
+	Name string
+	First string
+	Last string
+	Input string
+}
+
+func main() {
+
+	home := Person{
+		Name: "Student Learning Golang",
+		First: "David",
+		Last: "Hang",
+		Input: `<script>alert("Error");</script>`,
+	}
+
+	tpl, err := template.ParseFiles("tpl.gohtml")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.Execute(os.Stdout, home)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
