a48783fbc5851fce3008e4ad969d35bf9116be68
davishang <davishang@mail.fresnostate.edu>
2016-03-05 19:08:55 -0800

    request-FormValue-txtFile

---
+++ Request Handling/request-FormValue-txtFile/main.go
@@ -0,0 +1,56 @@
+// Davis Hang
+
+// Create a webpage that serves a form and allows the user to upload a txt file.
+// You do not need to check if the file is a txt; bad programming but just
+// trust the user to follow the instructions. Once a user has uploaded a txt
+// file, copy the text from the file and display it on the webpage.
+// Use req.FormFile and io.Copy to do this
+
+package main
+
+import (
+	"io"
+	"os"
+	"log"
+	"net/http"
+	"html/template"
+	"path/filepath"
+)
+
+func main(){
+	// parse template
+	tpl, err := template.ParseFiles("form.gohtml")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// handler
+	http.HandleFunc("/", func(res http.ResponseWriter,req *http.Request) {
+		// receive form submission
+		if req.Method == "POST" {
+			src, _, err := req.FormFile("file")
+			if err != nil {
+				panic(err)
+			}
+			defer src.Close()
+			//
+			dst, err := os.Create(filepath.Join("./", "file.txt"))
+			if err != nil {
+				http.Error(res, err.Error(), 500)
+				return
+			}
+			defer dst.Close()
+			io.Copy(dst, src)
+		}
+
+		// execute template
+		err = tpl.Execute(res, nil)
+		if err != nil {
+			http.Error(res, err.Error(), 500)
+			log.Println(err)
+		}
+	})
+	log.Println("Listening...")
+	http.ListenAndServe(":8080",nil)
+}
+
